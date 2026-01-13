eb83321b41a560b3e460d5f92563c0529ce8c2aa
wmiq84 <michaelwang3489@gmail.com>
2026-01-12 19:22:39 -0800

    Initialize flat grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello World";
+    int width = 25;
+    int height = 25;
+
+    std::vector<int> v = {};
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            v.push_back((i * width + j));
+        }
+    }
+
+    for (int val : v) {
+        std::cout << val << ' ';
+    }
 
     return 0;
 }
